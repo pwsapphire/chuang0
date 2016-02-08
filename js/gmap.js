3f3c99691360981627e09a07f75a2de7ae8df5c8
David Deltgen <David@PrometheusX.local>
2016-02-08 13:51:24 +0100

    update php backend and javascript to search and inject coords in gmaps

--- js/gmap.js
+++ js/gmap.js
@@ -8,20 +8,45 @@ function createFakeMarkers() {
     }
 }
 
+function fetchGPSCoords(theUrlParam){
+    $.ajax({
+        url: 'https://localhost:85/Yelp/php/backend.php',
+        data: "address=" + theUrlParam,
+        method: "GET",
+        dataType: "json",
+        "success": function(resp){
+            console.log(resp);
+            return resp;
+        }
+    });
+}
+
 
 function initGmap() {
-    createFakeMarkers();
+    //createFakeMarkers();
+    
+    var testAddress = encodeURIComponent("69, rue du clopp Rodange Luxembourg");
+    var gpsCoords = fetchGPSCoords(testAddress);
+    
     var theMap = new google.maps.Map(document.getElementById('gMap'), {
         center: { lat: 49.501413, lng: 5.951193 },
         scrollwheel: true,
         zoom: 6
     });
 
-    for (var gpsCoords in fakeMarkers) {
+    /*for (var gpsCoords in fakeMarkers) {
         new google.maps.Marker({
             position: fakeMarkers[gpsCoords],
             map: theMap,
             title: 'Test_' + fakeMarkers[gpsCoords],
         });
-    }
+    }*/
+    
+    new google.maps.Marker({
+            position: gpsCoords,
+            map: theMap,
+            title: 'Test',
+        });
+    
+    
 }
--- php/backend.php
+++ php/backend.php
@@ -1,1 +1,20 @@
+<?php 
 /*Fonction permettant de recuperer les coordonnées GPS à partir d'une adresse complète soumise au servce de Google Maps*/
+
+function getGPSCoords($theAddress) {
+    $myAPIKey = 'AIzaSyCH89EKdimLeM7wXrAfhbCvwOU6VBQbaYc';
+    $formattedAddress = urlencode($theAddress);
+    //$formattedAddress = $theAddress;
+    
+    //print_r($formattedAddress);
+    
+    $requestLink = "https://maps.googleapis.com/maps/api/geocode/json?address={$formattedAddress}&key={$myAPIKey}";
+    $theAnswer = file_get_contents($requestLink);
+    $gpsCoords = json_decode($theAnswer,TRUE)['results'][0]['geometry']['location'];
+    $googleCoords = "lat:{$gpsCoords['lat']}, lng:{$gpsCoords['lng']}";
+    //print_r($googleCoords);
+    return json_encode($gpsCoords);
+}
+
+?>
+<?php echo getGPSCoords($_GET['address']); ?>
