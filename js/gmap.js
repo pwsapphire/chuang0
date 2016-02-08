ce1792ba7ca7ee241a972c07fcbd0f72f943223a
David Deltgen <David@PrometheusX.local>
2016-02-08 14:19:06 +0100

    ajax Call  from gmap.js can display a marker

--- js/gmap.js
+++ js/gmap.js
@@ -1,24 +1,49 @@
 var fakeMarkers = [];
+var goodMarkers = [];
 
 function createFakeMarkers() {
     for (var index = 0; index < 10; index++) {
-        var latitude = Math.random() * (50-49) + 49;
-        var longitude = Math.random() * (7-5) + 5;
+        var latitude = Math.random() * (50 - 49) + 49;
+        var longitude = Math.random() * (7 - 5) + 5;
         fakeMarkers.push({ lat: latitude, lng: longitude });
     }
 }
 
-function fetchGPSCoords(theUrlParam){
+function fetchGPSCoords(theUrlParam) {
     $.ajax({
         url: 'https://localhost:85/Yelp/php/backend.php',
         data: "address=" + theUrlParam,
         method: "GET",
         dataType: "json",
-        "success": function(resp){
+        success: function (resp) {
             console.log(resp);
-            return resp;
+            goodMarkers.push(resp);
+            coordsFound = true;
+        },
+        complete: function(){
+            drawMarkers();
         }
     });
+}
+
+
+function drawMarkers() {
+    theMarkersToDraw = goodMarkers;
+    
+
+    var theMap = new google.maps.Map(document.getElementById('gMap'), {
+        center: { lat: 49.501413, lng: 5.951193 },
+        scrollwheel: true,
+        zoom: 6
+    });
+    
+    for (var gpsCoords in theMarkersToDraw) {
+        new google.maps.Marker({
+            position: theMarkersToDraw[gpsCoords],
+            map: theMap,
+            title: 'Test_' + theMarkersToDraw[gpsCoords],
+        });
+    }
 }
 
 
@@ -26,27 +51,6 @@ function initGmap() {
     //createFakeMarkers();
     
     var testAddress = encodeURIComponent("69, rue du clopp Rodange Luxembourg");
-    var gpsCoords = fetchGPSCoords(testAddress);
-    
-    var theMap = new google.maps.Map(document.getElementById('gMap'), {
-        center: { lat: 49.501413, lng: 5.951193 },
-        scrollwheel: true,
-        zoom: 6
-    });
 
-    /*for (var gpsCoords in fakeMarkers) {
-        new google.maps.Marker({
-            position: fakeMarkers[gpsCoords],
-            map: theMap,
-            title: 'Test_' + fakeMarkers[gpsCoords],
-        });
-    }*/
-    
-    new google.maps.Marker({
-            position: gpsCoords,
-            map: theMap,
-            title: 'Test',
-        });
-    
-    
+    fetchGPSCoords(testAddress);
 }
