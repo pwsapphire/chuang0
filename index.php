e36066b55029dbef120d7b65e7e421ea0e0be360
pwsapphire <williampan1981@gmail.com>
2016-02-09 16:27:43 +0100

    css

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 require 'inc/header.php';
 ?>
+		<main>
         <!-- Emplacement du header général contenant tous les liens vers les différentes parties du site  -->        
         <!-- Emplacement Google Maps -->
-        <div id="gMap"></div> 
+        <!--<div id="gMap"></div> -->
         <!-- Partie dédiée a la récupération de differentes "Places" via l'API Google, et leur affichage -->
-        <div id="gMapPlaces"></div>  
+        <!--<div id="gMapPlaces"></div>-->  
         <!-- Emplacement du slider avec les 10 derniers lieux: BONUS -->
+        </main>
 <?php
 require 'inc/footer.php';
 ?>
