05c051490e63aad4d4958312a0deb3a83882c06f
pwsapphire <williampan1981@gmail.com>
2016-02-09 15:31:56 +0100

    css

--- inc/header.php
+++ inc/header.php
@@ -4,16 +4,16 @@
         <meta charset="utf-8"/>
         <link rel="stylesheet" type="text/css" href="css/reset.css">
         <link rel="stylesheet" type="text/css" href="css/mainStyle.css">
-
+        <link href='https://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
     </head>
     <body>
 		<header>
+			<h1>Y</h1><h1>elp</h1>
 		    <nav>
 		        <ul>
-		            <li><a href="#">Accueil</a></li>
-		            <li><a href="#">Gestion des lieux</a></li>
-		            <li><a href="#">Gestion des utilisateurs</a></li>
-		            <li><a href="#">Login/logout</a></li>
+		            <li><a href="#">Home</a></li>
+		            <li><a href="#">Local Mamagement</a></li>
+		            <li><a href="#">User Management</a></li>
 		        </ul>
 		    </nav>
 		</header>
