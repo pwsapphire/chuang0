126f4fe93070259937a6c1bf9ca954d5d4133e17
pwsapphire <williampan1981@gmail.com>
2016-02-10 16:42:16 +0100

    en regle

--- index.php
+++ index.php
@@ -4,12 +4,28 @@ require 'inc/header.php';
 <main>
     <!-- Emplacement du header général contenant tous les liens vers les différentes parties du site  -->
     <!-- Emplacement Google Maps -->
+    <div id="gMapc">
+    	<div id="gap"></div>
+    </div>
     <!--<div id="gMap"></div> -->
     <!-- Partie dédiée a la récupération de differentes "Places" via l'API Google, et leur affichage -->
     <!--<div id="gMapPlaces"></div>-->
 
     <!-- Emplacement du slider avec les 10 derniers lieux: BONUS -->
-    <div id="theSlider"></div>
+    <div id="theSlider">
+    	<ul>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="d4845c99bbb50d64215da1f7c8cc9dd15d861376"><a href="#">Mosconi</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="c47f8b2b6c6a0e869588d22dba71ff88cb90f2aa"><a href="#">Logis Hotel Restaurant Dimmer</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="ddc6ab6420f78a0a28a6d8fd42f680c5daa552d5"><a href="#">Hotel St Fiacre</a></li>
+    		<!--<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="25dddb9bdb2c529ba5fc68399f708494c543edbe"><a href="#">Bel Air</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="8d73d36e693cc5d19794ad8871d09b48824d3120"><a href="#">Hôtel Ibis Luxembourg Aéroport</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="11eac3f4c2e7965c626b7a74183b8c4166b5365c"><a href="#">Hotel du Parc</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="16a6a35d5a25efb832ad1b7efabce7f1cbc2fce4"><a href="#">Mercure Grand Hotel Alfa Luxembourg</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="0ff23f0cb719b98a8a786d9363a2306f388b3d86"><a href="#">PARC PLAZA</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="d97258180e53024e5f895084aead42d8207feca7"><a href="#">Hotel Brimer</a></li>
+    		<li class="sliderThumb"><img src="img/fakeResto.jpg" alt="5daf6acc6b7b7c534a412fb09a9ab07778e19b7f"><a href="#">CANTINE Châtelet</a></li>-->
+    	</ul>
+    </div>
 </main>
 <?php 
 require 'inc/footer.php';
